607ba57ae4d211fc890c7c56ea35c9c11b5b2dae
Jon Hood <squinky86@gmail.com>
2018-09-06 09:58:33 -0500

    add correct solution comments

--- CPP/CWE-824/1/bad.cpp
+++ CPP/CWE-824/1/bad.cpp
@@ -11,6 +11,10 @@ int main(int argc, char *argv[])
 {
 	//take control of an uninitialized pointer
 	shared_ptr<bool> test1 (new bool);
+	//correct way to do this:
+	//shared_ptr<bool> test1 = make_shared<bool>();
+	//or, more concisely:
+	//auto test1 = make_shared<bool>();
 
 	//dereference uninitialized pointer
 	cout << "1: " << *test1 << endl;
