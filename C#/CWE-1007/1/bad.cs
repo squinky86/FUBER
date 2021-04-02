b5466dacca9c93dc2a709388e27b4aaf2fa2142b
Jon Hood <squinky86@gmail.com>
2021-04-02 08:31:09 -0500

    add CWE-1007 test case

---
+++ C#/CWE-1007/1/bad.cs
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2021, Jon Hood www.hoodsecurity.com
+ * License: ISC
+ */
+
+using System;
+
+namespace BadWeakness
+{
+	class Bad
+	{
+		static void Main()
+		{
+			Console.WriteLine("Please donate to this project at: https://paypal.com∕totally-legit-site.hoodsecurity.com/.");
+		}
+	}
+}
